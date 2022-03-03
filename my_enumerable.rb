4e1bba7cea0047d2ec1ee598bf667413e3669be6
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-03 17:59:54 +0200

    adde test

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -6,4 +6,16 @@ module MyEnumerable
     end
     result
   end
+
+  def ay?(&number)
+    result = true
+    each { |item| result = true if number.call(item)}
+    result
+  end
+
+  def filter(&number)
+    result = true
+    each { |item| result << item if number.call(item)}
+    result
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -13,4 +13,19 @@ class MyList
 end
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all?) { |e| e <= 5 } # true
+
+# Test #all?
+list.all? { |e| e < 5 }
+# => true
+list.all? { |e| e > 5 }
+# => false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# => true
+list.any? { |e| e == 5 }
+# => false
+
+# Test #filter
+list.filter(&:even?)
+# => [2, 4]
