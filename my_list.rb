21d2b686d8b72e7259cd5fbcb9de98cdea1324c1
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-03-03 22:29:30 +0100

    Fix the error undefined method call' for nil:NilClass (NoMethodError)

--- my_list.rb
+++ my_list.rb
@@ -14,18 +14,8 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-# Test #all?
-puts(list.all?) { |e| e < 5 }
-# => true
-puts(list.all?) { |e| e > 5 }
-# => false
-
-# Test #any?
-puts(list.any?) { |e| e == 2 }
-# => true
-puts(list.any?) { |e| e == 5 }
-# => false
-
-# Test #filter
-puts list.filter(&:even?)
-# => [2, 4]
+p(list.all? { |e| e < 5 }) # true
+p(list.all? { |e| e > 5 }) # false
+p(list.any? { |e| e == 2 }) # true
+p(list.any? { |e| e == 5 }) # false
+p(list.filter(&:even?)) # [2, 4]
